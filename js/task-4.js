a36428a0d2dc09f54e4106c43378f516b0e19f66
orestpol <orest.polagnyuk@gmail.com>
2025-08-06 19:09:04 +0300

    fwfwfwfwewwfw

--- js/task-4.js
+++ js/task-4.js
@@ -42,5 +42,6 @@ const getTotalBalanceByGender = (users, gender) =>
   }
 ];
 
+
 console.log(getTotalBalanceByGender(clients, "male")); // 12053
 console.log(getTotalBalanceByGender(clients, "female")); // 8863
